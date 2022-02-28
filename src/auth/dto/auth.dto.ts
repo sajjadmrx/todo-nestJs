e1c35cd9bca56671b8355a2d0b11c7793a962dbd
sajjadmrx <sajjadzibaee102@gmail.com>
2022-02-28 11:20:32 +0330

    add class-validator & validate AuthDto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,20 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class AuthDto {
-  username: string;
-  password: string;
+  //! message:Optional
+  @MaxLength(15, /*{ message: 'Username must be at most 15 characters long' } */)
+  @MinLength(4)
+  @IsString()
+  @IsNotEmpty({
+    message: 'Username is required',
+  })
+  username: string
+
+
+
+  @MaxLength(15)
+  @MinLength(8)
+  @IsString()
+  @IsNotEmpty({ message: 'Password is required' })
+  password: string
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 
 (async () => {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(
+    new ValidationPipe({})
+  )
   await app.listen(3000)
 })()
